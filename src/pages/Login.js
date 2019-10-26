c845ac885ac517103be59f2b3841ca52de6a1508
Jose Afranio Volpato Junior <jose.avjunior@sp.senac.br>
2019-10-25 23:59:45 -0300

    preparado para começar

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View , Text } from  'react-native';
+
+
+export default function Login(){
+    return <View>
+                <Text> Olá AirCnC !!!!</Text>   
+           </View>   
+}
