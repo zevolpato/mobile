a0cec504ae0f6eab5eb6bbb4b0366f1421aeacba
Jose Afranio Volpato Junior <jose.avjunior@sp.senac.br>
2019-10-27 15:06:01 -0300

    login totalmente pronto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,121 @@
-import React from 'react';
-import { View , Text } from  'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage ,KeyboardAvoidingView , Text , Image, StyleSheet, TextInput, TouchableOpacity } from  'react-native';
+import logo from '../assets/logo.png';
+
+import api from '../services/api';
+
+export default function Login({ navigation }){
+    const[email, setEmail] = useState('');
+    const[techs, setTechs] = useState('');
+
+    useEffect(() => {
+         AsyncStorage.getItem('user').then(user => {
+                if(user) {
+                    navigation.navigate('List');
+                }
+
+         })
+
+    }, []);
+
+    async function handleSubmit(){
+        const response = await api.post('/sessions', {
+            email
+        })
+
+        const { _id } = response.data;
+    
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+        
+        navigation.navigate('List');  
+    }
 
 
-export default function Login(){
-    return <View>
-                <Text> Olá AirCnC !!!!</Text>   
-           </View>   
+    return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}> 
+                <Image source={logo} />
+                <Text> Te amo...</Text>  
+                <View style={styles.form}>
+                <Text style={styles.label}>SEU EMAIL * </Text>
+                <TextInput
+                    style={styles.input} 
+                    placeholder="Seu Email"
+                    placeholderTextColor= "#999"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={email}
+                    onChangeText={setEmail}
+    />
+                <Text style={styles.label}>TECNOLOGIAS * </Text>
+                <TextInput
+                    style={styles.input} 
+                    placeholder="TECNOLOGIAS DE INTERESSE"
+                    placeholderTextColor= "#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
+                />
+                <TouchableOpacity  onPress={handleSubmit} style={styles.button}>
+                    <Text style={styles.buttonText}>Encontrar Spots</Text>
+                </TouchableOpacity>
+
+                </View>
+    </KeyboardAvoidingView>
+    )   
 }
+
+const styles  = StyleSheet.create({
+    
+    container: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 30,
+
+    },
+
+   
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+    },
+
+   input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444', 
+    height: 44,
+    marginBottom: 20, 
+    borderRadius: 2, 
+   },
+
+   button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    borderRadius: 2,
+
+   },
+
+
+   buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+    textAlign: 'center',
+
+   },
+
+
+});
